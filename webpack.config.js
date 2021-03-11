dd2078ab31f70af343588d0fa9e809eff9576909
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-03-11 17:53:22 +0200

    loaders configure

--- webpack.config.js
+++ webpack.config.js
@@ -24,14 +24,34 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "index.html",
     }),
-    new CopyPlugin([
-      {
-        from: path.resolve(__dirname, "src/favicon.ico"),
-        to: path.resolve(__dirname, "dist"),
-      },
-    ]),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
     new MiniCssExtractPlugin({
       filename: "bundle.[hash].css",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
 };
